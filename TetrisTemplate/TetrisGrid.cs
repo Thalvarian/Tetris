fcd89b4c4297dd0349a5f05543043eb8c2920023
Thalvarian <a.vanterwisga@students.uu.nl>
2017-10-10 15:58:19 +0200

    10 Okt
    Grid werkt, screensize aangepast

---
+++ TetrisTemplate/TetrisGrid.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+/*
+ * a class for representing the Tetris playing grid
+ */
+class TetrisGrid
+{
+    public TetrisGrid(Texture2D b)
+    {
+        gridblock = b;
+        position = Vector2.Zero;
+        this.Clear();
+    }
+    /*
+     * sprite for representing a single grid block
+     */
+    static Texture2D gridblock;
+
+
+    const int Width = 12;
+    const int Height = 20;
+    
+    /*
+ * the position of the tetris grid
+ */
+    Vector2 position;
+    int[,] TGrid;
+
+   
+    /*
+     * clears the grid
+     */
+    public void Clear()
+    {
+    }
+
+    /*
+     * draws the grid on the screen
+     */
+    public void Draw(GameTime gameTime, SpriteBatch s)
+        {
+        
+        TGrid = new int[Width, Height];
+        
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {   
+                Vector2 position = new Vector2(12 + x * gridblock.Width, 20 + y * gridblock.Width);
+                s.Draw(gridblock, position, Color.White);
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ TetrisTemplate/Tetromino.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+
+class Tetromino
+{
+    Texture2D TetBlock;
+    Vector2 Tetposition, velocity;
+
+//    public void Update(GameTime, gameTime)
+//    {
+//        Tetpostition += (velocity + new Vector2(velocityOffset, 0)) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+//    }
+}
+
+
+
